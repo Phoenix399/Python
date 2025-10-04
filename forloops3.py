cd1aedc0868ce6f7bc3cd8caa8046da65a89fdd5
Nutifafa Yaw Awuye-Kpobi <fafaawuye@outlook.com>
2025-10-04 12:28:30 +0000

    Update forloops3.py

--- forloops3.py
+++ forloops3.py
@@ -4,16 +4,10 @@
         #print("*", end=" ")
     #print()
 
-#print("star pattern")
-#n=int(input("Enter the limit: "))
-#for i in range(1,n+1):
- #   for j in range(i):
-  #      print("*", end=" ")
-   # print()
-
-print("sum of n natural numbers")
+print("star pattern")
 n=int(input("Enter the limit: "))
-sum=0
 for i in range(1,n+1):
-    sum = sum + i
-print("The sum is:", sum)   
+    for j in range(i):
+        print("*", end=" ")
+    print()
+
