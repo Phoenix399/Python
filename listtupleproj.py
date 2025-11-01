c8715d6644febed3d03c2b7e570150627321b955
Nutifafa Yaw Awuye-Kpobi <fafaawuye@outlook.com>
2025-11-01 13:43:08 +0000

    Add tuple creation and conversion to list

---
+++ listtupleproj.py
@@ -0,0 +1,9 @@
+# create a tuple
+my_tuple = (10, 20, 30, 40, 50)
+
+# convert tuple to list
+my_list = list(my_tuple)
+
+# display the result
+print("Tuple:", my_tuple)
+print("Converted List:", my_list)
