dee32c383a7f2060a2a2eceef612ef9526bf7683
Nutifafa Yaw Awuye-Kpobi <fafaawuye@outlook.com>
2025-11-29 12:23:40 +0000

    Add files via upload

---
+++ asymoticnot.py
@@ -0,0 +1,11 @@
+#Program to show COnstant Time complexity
+
+def printnumber(n):
+    iteration=0
+    print("THe number entered is:", n)
+    iteration+=1
+    print("Total iterations done by the code is:", iteration)
+
+printnumber(10)
+printnumber(20)
+print("\n With any 'n' the time taken by our code wont change")
