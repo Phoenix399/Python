99d43e5df4ba314549579e77262761ab527d515f
Nutifafa Yaw Awuye-Kpobi <fafaawuye@outlook.com>
2025-12-14 12:04:22 +0000

    Add bitwise operations examples in bits.py

---
+++ bits.py
@@ -0,0 +1,30 @@
+num1 = 10
+num2 = 4
+
+#Using AND operator 
+print ("num1 & num2 =", num1 & num2)
+
+#Using OR operator
+print("\nnum1 | num2 =", num1 | num2)
+
+#USing NOT operator
+print("\n~num1" ==", ~num1")
+
+#Using XOR operrator
+print("\nnum ^ num2 = ", num1 ^ num2)
+
+
+num1 = 10
+num2 = 4
+
+#USing right shift operator on num1
+print("\nnum1 >> 1 =", num1 >> 1)
+
+#Using RIght shift operator on num2
+print("\nnum2 >> 1 = ", num2 >> 1)
+
+#Using LEft shift operator on num1 
+print("\nnum1 << 1 = ", num1 << 1)
+
+#USing left operator on num2
+print("\nnum2 << 1 = ", num2 << 1)
