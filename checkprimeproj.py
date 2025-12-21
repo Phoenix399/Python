b3ba79cbff71b68480ecf5b2ca320e5b7c446254
Nutifafa Yaw Awuye-Kpobi <fafaawuye@outlook.com>
2025-12-21 14:01:34 +0000

    Refactor function to check for prime numbers

--- checkprimeproj.py
+++ checkprimeproj.py
@@ -1,23 +1,24 @@
-# Return True if n is even else False (only for 2-digit numbers)
-def isEvenOdd(n):
+# Return True if n is prime else False (only for 2-digit numbers)
+def isPrime2Digit(n):
     # check if number is 2-digit
     if n < 10 or n > 99:
         return None
 
-    # XOR with 1 equals n+1 for even numbers
-    if (n ^ 1) == n + 1:
-        return True
-    else:
-        return False
+    # 2-digit prime check
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
 
 
 number = int(input("Enter a 2-digit number: "))
 
-result = isEvenOdd(number)
+result = isPrime2Digit(number)
 
 if result is None:
     print("Not a 2-digit number")
 elif result:
-    print(number, "is Even")
+    print(number, "is Prime")
 else:
-    print(number, "is Odd")
+    print(number, "is Not Prime")
+
